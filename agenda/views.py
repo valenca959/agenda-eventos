4fc75290f404c93ed438524958c709d478593c6a
valenca959 <rodrigovenedino@gmail.com>
2024-07-23 05:40:09 -0300

    feat: Atualizar importações e templates da aplicação de agenda

--- agenda/views.py
+++ agenda/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import eventos
+from agenda.models import eventos
+from django.template import loader
 
 # Create your views here.
 def index(request):
@@ -9,10 +10,4 @@ def index(request):
 
 def exibir_eventos(request):
     evento = eventos[0]
-    return HttpResponse(f'''
-    <html><h1>EVENTO: {evento.nome}</h1>
-    <p>Categoria: {evento.categoria}</p>
-    <p>Local: {evento.local}</p>
-    <p>Link: {evento.link}</p>
-    </html>
-    ''')
+    return render(request=request, template_name="agenda/exibir_evento.html", context={'evento': evento})
